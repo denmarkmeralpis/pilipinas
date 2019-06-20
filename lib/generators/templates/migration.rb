3ecac9a142e211a0fe3a45468ff97812b7134e59
Kenji Miyamoto <miyamotoken101@gmail.com>
2019-06-20 16:09:22 +0800

    Add new data.yml and modify migration

--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -1,27 +1,72 @@
 class CreateLocations < ActiveRecord::Migration<%= migration_version %>
   def change
-    create_table :pilipinas_locations do |t|
+    create_table :pilipinas_regions do |t|
+      t.bigint  :location_id
+      t.integer :lft
+      t.integer :rgt
+      t.string  :code
+      t.string  :name
+      t.string  :longitude
+      t.string  :latitude
+      t.timestamps default: Time.now
+    end
+
+    create_table :pilipinas_provinces do |t|
+      t.bigint  :location_id
       t.bigint  :parent_id
       t.integer :lft
       t.integer :rgt
-      t.string  :type
+      t.string  :code
+      t.string  :name
+      t.string  :longitude
+      t.string  :latitude
+      t.timestamps default: Time.now
+    end
+
+    create_table :pilipinas_cities do |t|
+      t.bigint  :location_id
+      t.bigint  :parent_id
+      t.integer :lft
+      t.integer :rgt
       t.string  :code
       t.string  :name
       t.boolean :city, default: false
       t.string  :income_class
       t.string  :urban_rural
       t.string  :district
-      t.string  :postcode
       t.string  :longitude
       t.string  :latitude
-      t.bigint  :location_id
       t.timestamps default: Time.now
     end
 
-    add_index :pilipinas_locations, :location_id
-    add_index :pilipinas_locations, :code
-    add_index :pilipinas_locations, :parent_id
-    add_index :pilipinas_locations, :postcode
-    add_index :pilipinas_locations, [:name, :location_type, :postcode], name: 'index_pilipinas_locations_on_name_type_postcode'
+    create_table :pilipinas_barangays do |t|
+      t.bigint  :location_id
+      t.bigint  :parent_id
+      t.integer :lft
+      t.integer :rgt
+      t.string  :code
+      t.string  :name
+      t.string  :urban_rural
+      t.timestamps default: Time.now
+    end
+
+    add_index :pilipinas_regions, :location_id, unique: true
+    add_index :pilipinas_regions, :code
+    add_index :pilipinas_regions, :rgt
+
+    add_index :pilipinas_provinces, :location_id, unique: true
+    add_index :pilipinas_provinces, :code
+    add_index :pilipinas_provinces, :parent_id
+    add_index :pilipinas_provinces, :rgt
+
+    add_index :pilipinas_cities, :location_id, unique: true
+    add_index :pilipinas_cities, :code
+    add_index :pilipinas_cities, :parent_id
+    add_index :pilipinas_cities, :rgt
+
+    add_index :pilipinas_barangays, :location_id, unique: true
+    add_index :pilipinas_barangays, :code
+    add_index :pilipinas_barangays, :parent_id
+    add_index :pilipinas_barangays, :rgt
   end
 end
